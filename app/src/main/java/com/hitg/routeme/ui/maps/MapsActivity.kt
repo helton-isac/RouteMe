97b720540b02f52f14ecf928daa5fd30ff1a940e
Helton Isac <helton.isac@gmail.com>
2020-12-19 16:17:04 -0300

    Adding Edit Text to the Map Activity

--- app/src/main/java/com/hitg/routeme/ui/maps/MapsActivity.kt
+++ app/src/main/java/com/hitg/routeme/ui/maps/MapsActivity.kt
@@ -1,14 +1,15 @@
-package com.hitg.routeme
+package com.hitg.routeme.ui.maps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
 import com.google.android.gms.maps.OnMapReadyCallback
 import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
+import com.hitg.routeme.R
+
 
 class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
 
@@ -19,9 +20,10 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
+
 
     /**
      * Manipulates the map once available.
